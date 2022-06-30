c2f41cb3b0a8f7d00d51b0ee159a1ce104025135
Kevin <103957894+kevlarsov@users.noreply.github.com>
2022-06-30 08:47:27 -0600

    dog class (TA step)

---
+++ DogClass.playground/Contents.swift
@@ -0,0 +1,24 @@
+class Dog {
+    var breed = "Poodle"
+    var color = "White"
+    var height = 1
+    var weight = 40
+    
+    func shake() {
+        print("*SHAKES*")
+    }
+    
+    func sit() {
+        print("*SITS*")
+    }
+    
+    func layDown() {
+        print("*LAYS DOWN*")
+    }
+}
+
+var hound = Dog()
+hound.breed = "Hound"
+hound.color = "Brown"
+hound.height = 2
+hound.weight = 60
