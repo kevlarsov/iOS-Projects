500b7708646f833410e6cd87e6c341cfbeb0df1b
Kevin <103957894+kevlarsov@users.noreply.github.com>
2022-07-05 20:52:26 -0600

    directory cleanup and new Tic-Tac-Toe project

---
+++ Tic-Tac-Toe/Tic-Tac-Toe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tic-Tac-Toe
+//
+//  Created by Kevin Cottrell on 7/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
