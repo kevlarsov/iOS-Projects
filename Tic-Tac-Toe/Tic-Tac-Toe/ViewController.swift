a6e6ab9befe8a029c430b51659123dea238dce2e
Kevin <103957894+kevlarsov@users.noreply.github.com>
2022-07-07 14:12:26 -0600

    ??

--- Tic-Tac-Toe/Tic-Tac-Toe/ViewController.swift
+++ Tic-Tac-Toe/Tic-Tac-Toe/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
     var gameIsActive = true
     let winningCombinations = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
     
-    @IBAction func action(_ sender: UIButton) {
+    @IBAction func action(_ sender: AnyObject) {
         if (gameState[sender.tag-1] == 0 && gameIsActive == true) {
             gameState[sender.tag-1] = activePlayer
             if (activePlayer == 1) {
@@ -30,39 +30,35 @@ class ViewController: UIViewController {
         
         for combination in winningCombinations {
             if gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]] {
-                gameIsActive = false
-                if gameState[combination[0]] == 1 {
-                    winLabel.text = "X has won!"
-                } else {
-                    winLabel.text = "O has won!"
-                }
-                
-                if gameIsActive == true {
-                    for i in gameState {
-                        count = i * count
+                    gameIsActive = false
+                    if gameState[combination[0]] == 1 {
+                        winLabel.text = "X has won!"
+                    } else {
+                        winLabel.text = "O has won!"
                     }
-                    if count != 0 {
-                        winLabel.text = "It was a draw!"
-                        winLabel.isHidden = false
-                        resetButton.isHidden = false
+                    
+                    if gameIsActive == true {
+                        for i in gameState {
+                            count = i * count
+                        }
+                        if count != 0 {
+                            winLabel.text = "It was a draw!"
+                        }
                     }
                 }
-                resetButton.isHidden = false
-                winLabel.isHidden = false
-            }
         }
     }
     
     @IBOutlet weak var resetButton: UIButton!
-    @IBAction func playAgain(_ sender: UIButton) {
+    @IBAction func playAgain(_ sender: Any) {
         gameState = [0,0,0,0,0,0,0,0,0]
         gameIsActive = true
         activePlayer = 1
         winLabel.text = "Tic-Tac-Toe"
         
         for i in 1...9 {
-            let button = view.viewWithTag(i) as! UIButton
-            button.setImage(nil, for: UIControl.State())
+            let btn = view.viewWithTag(i) as! UIButton
+            btn.setImage(nil, for: UIControl.State())
         }
     }
     
