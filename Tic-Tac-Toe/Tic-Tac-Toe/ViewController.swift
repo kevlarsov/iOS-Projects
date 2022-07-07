eb00631fafa6d3ff30a8d47a2ab0de7c71c1396a
Kevin <103957894+kevlarsov@users.noreply.github.com>
2022-07-07 10:25:46 -0600

    fixes

--- Tic-Tac-Toe/Tic-Tac-Toe/ViewController.swift
+++ Tic-Tac-Toe/Tic-Tac-Toe/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
     var gameIsActive = true
     let winningCombinations = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
     
-    @IBAction func xBtn(_ sender: AnyObject) {
+    @IBAction func action(_ sender: UIButton) {
         if (gameState[sender.tag-1] == 0 && gameIsActive == true) {
             gameState[sender.tag-1] = activePlayer
             if (activePlayer == 1) {
@@ -54,19 +54,17 @@ class ViewController: UIViewController {
     }
     
     @IBOutlet weak var resetButton: UIButton!
-    @IBAction func playAgain(_ sender: Any) {
+    @IBAction func playAgain(_ sender: UIButton) {
         gameState = [0,0,0,0,0,0,0,0,0]
         gameIsActive = true
         activePlayer = 1
-        resetButton.isHidden = true
-        winLabel.isHidden = true
+        winLabel.text = "Tic-Tac-Toe"
         
         for i in 1...9 {
             let button = view.viewWithTag(i) as! UIButton
             button.setImage(nil, for: UIControl.State())
         }
     }
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
